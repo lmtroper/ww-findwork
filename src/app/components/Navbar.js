055011332d131949ed7f8a7976c57441b2086161
Larissa Troper <lmtroper@uwaterloo.ca>
2024-07-04 09:46:18 -0400

    changed flow to start with questionnaire p1

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-const Navbar = ({ signup }) => {
+const Navbar = () => {
     const pathname = usePathname();
 
   return (
@@ -13,11 +13,11 @@ const Navbar = ({ signup }) => {
                 <img src="/logo.png" className='min-w-[80px] max-w-[80px] mx-[15px]'/>
             </Link>
         </div>
-        <div className={`${ signup ? 'hidden' : 'flex' } w-full flex-col items-center mt-10 ${pathname !== '/job-list' ? 'active-tab': ''}`}>
+        <div className={`w-full flex flex-col items-center mt-10 ${pathname !== '/job-list' ? 'active-tab': ''}`}>
             <img src="/questionnaire.png" className='min-w-[40px] max-w-[40px]' />
             <div className='items-center font-medium mt-2'>Questionnaire</div>
         </div>
-        <div className={`${ signup ? 'hidden' : 'flex' } w-full flex-col items-center mt-10 ${pathname === '/job-list' ? 'active-tab': ''}`}>
+        <div className={`w-full flex flex-col items-center mt-10 ${pathname === '/job-list' ? 'active-tab': ''}`}>
             <img src="/results.png" className='min-w-[40px] max-w-[40px]' />
             <div className='items-center font-medium mt-2'>Results</div>
         </div>
