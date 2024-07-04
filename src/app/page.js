5d591727985e6e6db1e6d9013cc00641292c1d71
Larissa Troper <lmtroper@uwaterloo.ca>
2024-07-04 09:53:24 -0400

    upload resume functionality

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,24 @@ const bakbakOne = Bakbak_One({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   const [username, setUsername] = useState('Sirisha')
+  const [resume, setResume] = useState(null)
+  const [resumeURL, setResumeURL] = useState(null)
+
+
+  const handleResumeUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setResume(file);
+      const url = URL.createObjectURL(file);
+      setResumeURL(url);
+    }
+  };
+
+  const handeNext = () => {
+    // handle the next button click here
+    // TO-DO: parse through resume, extract skills section
+  }
+
 
   return (
     <div className='w-full absolute top-[200px] flex flex-col items-center justify-center px-10'>
@@ -25,10 +43,24 @@ export default function Home() {
           Don’t worry you can come back later to change this.
         </div>
     </div>
-    <button className='mb-5 resume-upload-btn w-[640px] flex justify-center items-center'>
-      <Image src="/upload.png" alt="upload" width={25} height={25} />
-      <div className='ml-2'>Click here to upload your resume</div>
-    </button>
+    {resume && (
+        <div className='mb-5'>
+          <a href={resumeURL} target="_blank" rel="noopener noreferrer" className='text-blue-500 underline'>
+            {resume.name}
+          </a>
+        </div>
+      )}
+    <label htmlFor="resume-upload" className='mb-5 resume-upload-btn w-[640px] flex justify-center items-center cursor-pointer'>
+        <Image src="/upload.png" alt="upload" width={25} height={25} />
+        <div className='ml-2'>Click here to upload your resume</div>
+      </label>
+      <input 
+        id="resume-upload" 
+        type="file" 
+        accept=".pdf,.doc,.docx" 
+        style={{ display: 'none' }} 
+        onChange={handleResumeUpload} 
+      />
     <Link href="questionnaire2">
         <button className='signin-button w-[640px]'>Start questionnaire</button>
     </Link>
